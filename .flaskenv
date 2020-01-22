ce300c652250ab3bc27bee94a2ba6e3a3b8c534d
yotsaphonSutweha <komsan_58@hotmail.com>
2020-01-22 15:29:37 +0000

    fix4

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
 FLASK_APP=Controllers
-FLASK_ENV=production 
+FLASK_ENV=development 
--- Controllers/__init__.py
+++ Controllers/__init__.py
@@ -2,15 +2,12 @@ from flask import Flask
 from .extensions import mongo
 from .main import main
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.config['MONGODB_SETTINGS'] = {
-        'db' : 'hddt-database',
-        'host' : 'mongodb://oofdon2540:Asd45rty@ds157256.mlab.com:57256/hddt-database?retryWrites=false'
-    }
+app.config['MONGODB_SETTINGS'] = {
+    'db' : 'hddt-database',
+    'host' : 'mongodb://oofdon2540:Asd45rty@ds157256.mlab.com:57256/hddt-database?retryWrites=false'
+}
     
-    mongo.init_app(app)
-    app.register_blueprint(main)
-
-    return app
+mongo.init_app(app)
+app.register_blueprint(main)
