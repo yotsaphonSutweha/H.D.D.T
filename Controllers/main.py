c0ac37c50b48cd6612080186254574dcbf325f55
yotsaphonSutweha <komsan_58@hotmail.com>
2020-04-19 15:28:24 +0100

    added in registration details checking

--- Controllers/main.py
+++ Controllers/main.py
@@ -7,6 +7,7 @@ import bcrypt
 from flask_cors import cross_origin, CORS
 from flask import render_template, request, url_for, session, redirect, make_response
 from flask_json import as_json, json_response
+import re
 main = Blueprint('main', __name__)
 ops = Operations()
 import os 
@@ -84,41 +85,78 @@ def register():
         ward = request.json.get('ward')
         checking_doctor = ops.check_if_doctor_exist(employeeId)
         checking_nurse = ops.check_if_nurse_exist(employeeId)
+        print('password length {0}'.format(len(password)))
         if checking_doctor == True or checking_nurse == True:
             error_message = {
                 'message': 'The employee ID is already exists.'
             }
             return json_response(status_=422, data_ = error_message)
         else:
-            if jobRole == 'Doctor': 
-                hased_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-                ops.register_doctor (
-                    employeeId,
-                    hased_password,
-                    first_name,
-                    second_name,
-                    contact_number, 
-                    room_number,
-                    ward
-                )
-                response = make_response()
-                # response.set_cookie('hddt', 'signed_in_cookie', max_age=60*60)
-                session['employeeId'] = employeeId
-                return response
+            if password != confirm_password:
+                error_message = {
+                    'message': 'Password and comfirm password are not the same.'
+                }
+                return json_response(status_=422, data_ = error_message)
+            else:
+                if len(password) < 8 or not password.isalnum():
+                    error_message = {
+                        'message': 'Password must contain the minimum of 8 characters with the combination of letters or numbers.'
+                    }
+                    return json_response(status_=422, data_ = error_message)
+                else:
+                    if not contact_number.isdigit() or len(contact_number) > 10:
+                        error_message = {
+                            'message': 'The contact number must contain only digits and follow the Irish phone number standard ie. 08xxxxxxxx.'
+                            }
+                        return json_response(status_=422, data_ = error_message)
+                    else:
+                        if len(room_number) > 3:
+                            error_message = {
+                                'message': 'Room number must be less than 3 characters.'
+                            }
+                            return json_response(status_=422, data_ = error_message)
+                        else:
+                            if len(ward) > 10:
+                                error_message = {
+                                    'message': 'Please provide appropriate an ward location'
+                                }
+                                return json_response(status_=422, data_ =   error_message)
+                            else:
+                                if len(employeeId) > 8 or not password.isalnum():
+                                    error_message = {
+                                        'message': 'The Employee ID must be less than 8 characters with the combination of letters or numbers.'
+                                    }
+                                    return json_response(status_=422, data_ = error_message)
+                                else:
+                                    if jobRole == 'Doctor': 
+                                        hased_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                                        ops.register_doctor (
+                                            employeeId,
+                                            hased_password,
+                                            first_name,
+                                            second_name,
+                                            contact_number, 
+                                            room_number,
+                                            ward
+                                        )
+                                        response = make_response()
+                                        # response.set_cookie('hddt', 'signed_in_cookie', max_age=60*60)
+                                        session['employeeId'] = employeeId
+                                        return response
 
-            elif jobRole == 'Nurse':
-                hased_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-                ops.register_nurse (
-                    employeeId,
-                    hased_password,
-                    first_name,
-                    second_name,
-                    contact_number, 
-                    room_number,
-                    ward
-                )
-                response = make_response()
-                # response.set_cookie('hddt', 'signed_in_cookie', max_age=60*60)
-                session['employeeId'] = employeeId
-                return response
-        
+                                    elif jobRole == 'Nurse':
+                                        hased_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                                        ops.register_nurse (
+                                            employeeId,
+                                            hased_password,
+                                            first_name,
+                                            second_name,
+                                            contact_number, 
+                                            room_number,
+                                            ward
+                                        )
+                                        response = make_response()
+                                        # response.set_cookie('hddt', 'signed_in_cookie', max_age=60*60)
+                                        session['employeeId'] = employeeId
+                                        return response
+                                
