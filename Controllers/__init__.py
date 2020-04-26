39d575f8b2d906393d06dc2149d6a781ec75ccec
yotsaphonSutweha <komsan_58@hotmail.com>
2020-04-26 11:30:09 +0100

    revert init file back to before testing

--- Controllers/__init__.py
+++ Controllers/__init__.py
@@ -10,42 +10,63 @@ from datetime import timedelta
 from flask_cors import CORS
 import os
 
-def create_app(test_config=False):
-    app = Flask(__name__,
+# def create_app(test_config=False):
+#     app = Flask(__name__,
+#                 template_folder='../Views/templates',
+#                 static_url_path='',
+#                 static_folder='../Views/static')
+#     json = FlaskJSON()
+#     cors = CORS(resources={r"/api/*": {"origins": "*"}}, allow_headers=["Content-Type", "Accept", "X-Request-With", "access-control-allow-origin", "Access-Control-Allow-Credentials"], supports_credentials=True)
+#     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+#     if test_config == False:
+#         app.config['MONGODB_SETTINGS'] = {
+#             'db' : os.environ.get('MONGO_DB'),
+#             'host' : os.environ.get('MONGO_DB_URI')
+#         }
+#     elif test_config == True:
+#         app.config['MONGODB_SETTINGS'] = {
+#             'db' : os.environ.get('MONGO_TEST_NAME'),
+#             'host' : os.environ.get('MONGO_TEST_URI')
+#         }
+
+#     # app.config['MONGODB_SETTINGS'] = {
+#     #     'db' : os.environ.get('MONGO_TEST_NAME'),
+#     #     'host' : os.environ.get('MONGO_TEST_URI')
+#     # }
+
+#     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+#     mongo.init_app(app)
+#     # app.config['SESSION_COOKIE_HTTPONLY'] = True
+#     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)
+#     json.init_app(app)
+#     cors.init_app(app)
+#     app.register_blueprint(main)
+#     app.register_blueprint(diagnosis_controller)
+#     app.register_blueprint(patients_controller)
+#     app.register_blueprint(visualisation_controller)
+#     return app
+
+app = Flask(__name__,
                 template_folder='../Views/templates',
                 static_url_path='',
                 static_folder='../Views/static')
-    json = FlaskJSON()
-    cors = CORS(resources={r"/api/*": {"origins": "*"}}, allow_headers=["Content-Type", "Accept", "X-Request-With", "access-control-allow-origin", "Access-Control-Allow-Credentials"], supports_credentials=True)
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+json = FlaskJSON()
+cors = CORS(resources={r"/api/*": {"origins": "*"}}, allow_headers=["Content-Type", "Accept", "X-Request-With", "access-control-allow-origin", "Access-Control-Allow-Credentials"], supports_credentials=True)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['MONGODB_SETTINGS'] = {
+        'db' : os.environ.get('MONGO_DB'),
+        'host' : os.environ.get('MONGO_DB_URI')
+    }
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+mongo.init_app(app)
+# app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)
+json.init_app(app)
+cors.init_app(app)
+app.register_blueprint(main)
+app.register_blueprint(diagnosis_controller)
+app.register_blueprint(patients_controller)
+app.register_blueprint(visualisation_controller)
 
-
-    if test_config == False:
-        app.config['MONGODB_SETTINGS'] = {
-            'db' : os.environ.get('MONGO_DB'),
-            'host' : os.environ.get('MONGO_DB_URI')
-        }
-    elif test_config == True:
-        app.config['MONGODB_SETTINGS'] = {
-            'db' : os.environ.get('MONGO_TEST_NAME'),
-            'host' : os.environ.get('MONGO_TEST_URI')
-        }
-
-    # app.config['MONGODB_SETTINGS'] = {
-    #     'db' : os.environ.get('MONGO_TEST_NAME'),
-    #     'host' : os.environ.get('MONGO_TEST_URI')
-    # }
-
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    mongo.init_app(app)
-    # app.config['SESSION_COOKIE_HTTPONLY'] = True
-    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)
-    json.init_app(app)
-    cors.init_app(app)
-    app.register_blueprint(main)
-    app.register_blueprint(diagnosis_controller)
-    app.register_blueprint(patients_controller)
-    app.register_blueprint(visualisation_controller)
-    return app
-
-create_app()
